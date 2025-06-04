b6b6ba766498832ac5e639984bd5983aed554a69
wren <168173870+wrenSkl@users.noreply.github.com>
2025-06-04 14:17:33 +0100

    Create signal.py

---
+++ grimwalker/util/signal.py
@@ -0,0 +1,30 @@
+import sdl2, sdl2.ext
+from grimwalker._internal._globals import *
+
+class Signal:
+	def __init__(self, target_uid, signal_id):
+		self._target_uid = target_uid
+		self._signal_id = signal_id
+		self._target = self.__get_target()
+
+	def __get_target(self):
+		if self._target_uid:
+			return globals.uids[self._target_uid]
+		else:
+			raise AttributeError("grimwalker.util.signal.Signal._target_uid does not exist or is an invalid type")
+
+	@property
+	def target_uid(self):
+		return self._target_uid
+
+	@target_uid.setter
+	def target_uid(self, val):
+		self._target_uid = val
+
+	@property
+	def signal_id(self):
+		return self._signal_id
+
+	@target_uid.setter
+	def signal_id(self, val):
+		self._signal_id = val
